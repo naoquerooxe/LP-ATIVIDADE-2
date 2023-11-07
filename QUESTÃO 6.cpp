d2fa25e0eadbde9166c87e4ec3d104d5e140a834
Nicolie <143574745+naoquerooxe@users.noreply.github.com>
2023-11-07 15:35:51 -0300

    Add files via upload

---
+++ QUESTÃO 6.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+
+    setlocale(LC_ALL, "portuguese");
+
+    int opcao;
+
+    printf("   _____              \n");
+    printf("-> | 1 | NOVO JOGO    \n");
+    printf("   _____              \n");
+	printf("-> | 2 | CARREGAR JOGO\n");
+    printf("   _____              \n");
+	printf("-> | 3 | CONFIGURAÇÃOES\n");
+
+
+    printf("\n");
+	printf("Digite o código da sua opção:");
+    scanf("%i", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+    	printf("Carregando novo jogo... \n");
+    break;
+	case 2:
+	printf("Carregando jogo... \n");
+	break;
+	case 3:
+	printf("Entrando nas configurações... \n");
+	break;
+	default:
+		printf("Opção inválida. Tente Novamente. \n");
+		
+	break;
+}
+	
+	return 0;
+}
